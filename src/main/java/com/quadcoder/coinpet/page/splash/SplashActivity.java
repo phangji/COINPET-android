d8e66127558d440bff27846c6fe423feb21137f4
phangji <chuckychoki@gmail.com>
2015-04-15 19:47:50 +0900

    SplashActivity 생

---
+++ src/main/java/com/quadcoder/coinpet/page/splash/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.quadcoder.coinpet.page.splash;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.quadcoder.coinpet.MainActivity;
+import com.quadcoder.coinpet.R;
+
+public class SplashActivity extends Activity {
+
+    private static final int DELAY_TIME = 800;
+    Handler mHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, DELAY_TIME);
+    }
+}

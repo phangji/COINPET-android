47a9d875f3922f4a88ff790730cff88a1cc18123
phangji <chuckychoki@gmail.com>
2015-04-22 00:45:29 +0900

    가입 시 펫 나올 때, 구름 애니메이션 적용

--- src/main/java/com/quadcoder/coinpet/page/signup/SignupFirstFragment.java
+++ src/main/java/com/quadcoder/coinpet/page/signup/SignupFirstFragment.java
@@ -2,11 +2,15 @@ package com.quadcoder.coinpet.page.signup;
 
 
 import android.graphics.Typeface;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -29,6 +33,12 @@ public class SignupFirstFragment extends Fragment {
     ImageView imgvBox;
 
     @Override
+    public void onResume() {
+        super.onResume();
+        startAnimation();
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_signup_first, container, false);
@@ -39,6 +49,9 @@ public class SignupFirstFragment extends Fragment {
         Button btn = (Button)rootView.findViewById(R.id.btnNext);
         Typeface font = Typeface.createFromAsset(getActivity().getAssets(), Constants.FONT_NORMAL);
         btn.setTypeface(font);
+
+        imgvCloud1 = (ImageView)rootView.findViewById(R.id.imgvCloud1);
+        imgvCloud2 = (ImageView)rootView.findViewById(R.id.imgvCloud2);
 
         rootView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,5 +69,16 @@ public class SignupFirstFragment extends Fragment {
         });
 
         return rootView;
+    }
+    ImageView imgvCloud1;
+    ImageView imgvCloud2;
+
+    void startAnimation() {
+
+        Animation animCloud1 = AnimationUtils.loadAnimation(getActivity(), R.anim.cloud1_anim);
+        imgvCloud1.startAnimation(animCloud1);
+        Animation animCloud2 = AnimationUtils.loadAnimation(getActivity(), R.anim.cloud2_anim);
+        imgvCloud2.startAnimation(animCloud2);
+
     }
 }

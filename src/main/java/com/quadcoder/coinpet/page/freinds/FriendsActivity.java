9aea246a86843b1669c00a027ffb185f550728eb
phangji <chuckychoki@gmail.com>
2015-06-03 23:24:50 +0900

    implements audio sounds on FriendActivity

--- src/main/java/com/quadcoder/coinpet/page/freinds/FriendsActivity.java
+++ src/main/java/com/quadcoder/coinpet/page/freinds/FriendsActivity.java
@@ -4,15 +4,84 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.quadcoder.coinpet.R;
+import com.quadcoder.coinpet.audio.AudioEffect;
+import com.quadcoder.coinpet.database.DBManager;
+import com.quadcoder.coinpet.model.Friend;
+
+import java.util.ArrayList;
 
 public class FriendsActivity extends ActionBarActivity {
+
+    ImageView imgvMM;
+    ImageView imgvTT;
+    ImageView imgvKuKu;
+    ImageView imgvDD;
+    ImageView imgvKoKo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+
+        setLayouts();
+        setClickListeners();
+    }
+
+    final AudioEffect kukuAudio = new AudioEffect(AudioEffect.FREIND_KUKU);
+    final AudioEffect chchAudio = new AudioEffect(AudioEffect.FREIND_CHCH);
+    final AudioEffect ddAudio = new AudioEffect(AudioEffect.FREIND_DD);
+    final AudioEffect kokoAudio = new AudioEffect(AudioEffect.FREIND_KOKO);
+    final AudioEffect mamaAudio = new AudioEffect(AudioEffect.FREIND_MAMA);
+
+    private void setLayouts() {
+        imgvMM = (ImageView) findViewById(R.id.imgvMM);
+        imgvTT = (ImageView) findViewById(R.id.imgvTT);
+        imgvKuKu = (ImageView) findViewById(R.id.imgvKuKu);
+        imgvDD = (ImageView) findViewById(R.id.imgvDD);
+        imgvKoKo = (ImageView) findViewById(R.id.imgvKK);
+
+        ArrayList<Friend> friendList = DBManager.getInstance().getFriendList();
+    }
+
+    private void setClickListeners() {
+        imgvMM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                kukuAudio.play();
+            }
+        });
+
+        imgvTT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chchAudio.play();
+            }
+        });
+
+        imgvKuKu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mamaAudio.play();
+            }
+        });
+
+        imgvDD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ddAudio.play();
+            }
+        });
+
+        imgvKoKo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                kokoAudio.play();
+            }
+        });
     }
 
     @Override
